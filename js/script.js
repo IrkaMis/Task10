d30fcb146f9ce97a937f5436df9c6545e10390c0
Irka Mis <irka.mis.bdg@gmail.com>
2021-06-01 19:50:44 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
+// function getFullName
 function getFullName(firstName, secondName) {
     alert(firstName + " " + secondName);
   }
 
-  getFullName("Irena", "Miś");
+getFullName("Irena", "Miś");
+
+
+// function getPhoneNumber
+function getPhoneNumber(phoneNumber) {
+  if (typeof phoneNumber === "string" && phoneNumber.length >= 9) {
+    alert("+48" + phoneNumber);
+  } else {
+    alert("+48 phone error");
+  }
+}
+
+getPhoneNumber("123456789");
+getPhoneNumber(123456789);
+getPhoneNumber("12345678");
